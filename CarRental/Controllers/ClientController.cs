59a81ee316b67badf1c0a7964a3785d0ca45bef4
WAD00007922 <Suxrob.khatamov@gmail.com>
2021-04-10 22:02:41 +0500

    Add project files.

---
+++ CarRental/Controllers/ClientController.cs
@@ -0,0 +1,89 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: ClientController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ClientController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClientController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Client model)
+        {
+            
+                if(ModelState.IsValid) 
+                {
+                    return RedirectToAction(nameof(Index));
+
+                }
+            
+                return View(model);
+            
+        }
+
+        // GET: ClientController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClientController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClientController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClientController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CarRental/Models/Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Models
+{
+    public class Client : IValidatableObject
+    {
+        [DisplayName("Client ID")]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Date of birth")]
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [DisplayName("Address Line #3")]
+        public string AddressLine1 { get; set; }
+
+        [DisplayName("Address Line #2")]
+        public string AddressLine2 { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (DateTime.Now.AddYears(-18) <= DateOfBirth)
+            {
+                yield return new ValidationResult("Should be 18+", new[] { "DateOfBirth" });
+            }
+        }
+    }
+
+    public enum Gender
+    {
+        Female,
+        Male
+    }
+}
